0d8e0da99e06ac2a72f78e09ebf8ae946116dde6
John Emerson Requizo <jr.jemerson14@gmail.com>
2025-12-28 11:03:24 +1100

    Completed LD B, E instruction

--- src/test_decode.zig
+++ src/test_decode.zig
@@ -540,7 +540,7 @@ test "decode and execute 0x41 [LD B, C]" {
     try expect(register.E == 0);
     try expect(register.H == 0);
     try expect(register.L == 0);
-    try expect(register.IR == 0x10);
+    try expect(register.IR == 0x10); // STOP
 }
 
 test "decode and execute 0x42 [LD B, D]" {
@@ -562,6 +562,30 @@ test "decode and execute 0x42 [LD B, D]" {
     try expect(register.C == 0);
     try expect(register.D == 0x0f);
     try expect(register.E == 0);
+    try expect(register.H == 0);
+    try expect(register.L == 0);
+    try expect(register.IR == 0x10); // STOP
+}
+
+test "decode and execute 0x43 [LD B, E]" {
+    var register = RegisterFile{
+        .PC = 0x0100,
+        .IR = 0x43,
+        .E = 0xa5,
+    };
+
+    var memory = Memory.init();
+    memory.set(0x0100, 0x43);
+    memory.set(0x0101, 0x10);
+
+    try main.decodeAndExecute(&register, &memory);
+
+    try expect(register.PC == 0x0101);
+    try expect(register.A == 0);
+    try expect(register.B == 0xa5);
+    try expect(register.C == 0);
+    try expect(register.D == 0);
+    try expect(register.E == 0xa5);
     try expect(register.H == 0);
     try expect(register.L == 0);
     try expect(register.IR == 0x10);
