f94da3b8e6cc350832eb360344760172cf774eab
John Emerson Requizo <jr.jemerson14@gmail.com>
2025-12-29 18:32:20 +1100

    Completed LD L, C instruction

--- README.md
+++ README.md
@@ -88,7 +88,7 @@ Also because [System of Levers](https://www.youtube.com/watch?v=hy2yY5a1Z-0) tol
 - [x] 0x66 - LD H, (HL)
 - [x] 0x67 - LD H, A
 - [x] 0x68 - LD L, B
-- [ ] 0x69 - LD L, C
+- [x] 0x69 - LD L, C
 - [ ] 0x6a - LD L, D
 - [ ] 0x6b - LD L, E
 - [ ] 0x6c - LD L, H
--- src/test_decode.zig
+++ src/test_decode.zig
@@ -1588,3 +1588,29 @@ test "decode and execute 0x68 [LD L, B]" {
     try expect(register.L == 0xd8);
     try expect(register.IR == STOP_OP_CODE);
 }
+
+test "decode and execute 0x69 [LD L, C]" {
+    const op_code: u8 = 0x69;
+    const start_mem_location: u16 = 0x0100;
+
+    var register = RegisterFile{
+        .PC = start_mem_location,
+        .IR = op_code,
+        .C = 0x49
+    };
+
+    var memory = Memory.init();
+    memory.set(start_mem_location, op_code);
+    memory.set(start_mem_location + 1, STOP_OP_CODE);
+
+    try main.decodeAndExecute(&register, &memory);
+    try expect(register.PC == start_mem_location + 1);
+    try expect(register.A == 0);
+    try expect(register.B == 0);
+    try expect(register.C == 0x49);
+    try expect(register.D == 0);
+    try expect(register.E == 0);
+    try expect(register.H == 0);
+    try expect(register.L == 0x49);
+    try expect(register.IR == STOP_OP_CODE);
+}
