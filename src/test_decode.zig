9efd35b86876e5d0f1e22fb8792573026dee5c8f
John Emerson Requizo <jr.jemerson14@gmail.com>
2025-12-30 12:48:36 +1100

    Completed LD A, D instruction

--- src/test_decode.zig
+++ src/test_decode.zig
@@ -2025,3 +2025,29 @@ test "decode and execute 0x79 [LD A, C]" {
     try expect(register.L == 0);
     try expect(register.IR == STOP_OP_CODE);
 }
+
+test "decode and execute 0x7a [LD A, D]" {
+    const op_code: u8 = 0x7a;
+    const start_mem_location: u16 = 0x0100;
+
+    var register = RegisterFile{
+        .PC = start_mem_location,
+        .IR = op_code,
+        .D = 0x93
+    };
+
+    var memory = Memory.init();
+    memory.set(start_mem_location, op_code);
+    memory.set(start_mem_location + 1, STOP_OP_CODE);
+
+    try main.decodeAndExecute(&register, &memory);
+    try expect(register.PC == start_mem_location + 1);
+    try expect(register.A == 0x93);
+    try expect(register.B == 0);
+    try expect(register.C == 0);
+    try expect(register.D == 0x93);
+    try expect(register.E == 0);
+    try expect(register.H == 0);
+    try expect(register.L == 0);
+    try expect(register.IR == STOP_OP_CODE);
+}
