69d48b8b803c3393e4a8ad1896e7f9a779fd3997
John Emerson Requizo <jr.jemerson14@gmail.com>
2026-01-16 23:41:54 +1100

    Commiting some util functions

--- docs/logs/day-25.md
+++ docs/logs/day-25.md
@@ -5,3 +5,8 @@ I'm back now to properly refactor things. I'd also like to get some feedback
 from the zig community after this refactor.
 
 Total time: 85 minutes
+
+# Part two
+Just some more refactoring
+
+Total time: 31 minutes
---
+++ src/utils.zig
@@ -0,0 +1,34 @@
+const std = @import("std");
+
+const NEG_SIGN_MASK: u16 = 0x80; // 0b1000_0000
+const NEG_SIGN_EXT_MASK: u16 = 0xFF00;
+const POST_SIGN_EXT_MASK: u16 = 0x0000;
+
+pub fn signExtend(value: u16) u16 {
+    const sign: u16 = if ((value & NEG_SIGN_MASK) == NEG_SIGN_MASK) NEG_SIGN_EXT_MASK else POST_SIGN_EXT_MASK;
+    return (value | sign);
+}
+
+pub fn addOffset(value: u16, offset: u16) u16 {
+    const signExtendedOffset: u16 = signExtend(offset);
+    return @bitCast(@as(i16, @bitCast(value)) + @as(i16, @bitCast(signExtendedOffset)));
+}
+
+const expectEqual = std.testing.expectEqual;
+
+test "sign extend - negative two's complement" {
+    const negative_three_u8: u8 = 0b1111_1101;
+    const negative_three_i16: i16 = @bitCast(signExtend(negative_three_u8));
+
+    try expectEqual(-3, negative_three_i16);
+}
+
+test "sign extend - positive two's complement" {
+    const positive_two_u8: u8 = 0b0000_00010;
+    const positive_two_i16: i16 = @bitCast(signExtend(positive_two_u8));
+
+    try expectEqual(2, positive_two_i16);
+}
+
+test "addOffset - negative offset" {
+}
