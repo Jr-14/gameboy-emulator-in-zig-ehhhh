03cf3329eabf5cee335c3d23f68896149c1e33c3
John Emerson Requizo <jr.jemerson14@gmail.com>
2026-01-26 12:58:31 +1100

    I think I did something cool with function pointers and dynamic dispatching :D

--- src/instruction.zig
+++ src/instruction.zig
@@ -15,25 +15,42 @@ pub fn incrementRegister(
     const sum = utils.byteAdd(get(r), 1);
     set(r, sum.result);
     p.unsetFlag(.N);
-    if (sum.result == 0) {
-        p.setFlag(.Z);
-    }
-    if (sum.half_carry == 1) {
-        p.setFlag(.H);
-    }
+    if (sum.result == 0) p.setFlag(.Z) else p.unsetFlag(.Z);
+    if (sum.half_carry == 1) p.setFlag(.H) else p.unsetFlag(.H);
 }
 
 const expectEqual = std.testing.expectEqual;
 
-test "incrementRegister" {
+test "incrementRegister, BC" {
+
     var memory: Memory = .init();
     var processor: Processor = .init(&memory);
 
-    var BC = &processor.BC;
-    incrementRegister(&processor, BC, Register.setHi, Register.getHi);
+    incrementRegister(&processor, &processor.BC, Register.setHi, Register.getHi);
 
-    try expectEqual(1, BC.getHi());
+    try expectEqual(0x01, processor.BC.getHi());
     try expectEqual(false, processor.isFlagSet(.Z));
     try expectEqual(false, processor.isFlagSet(.N));
     try expectEqual(false, processor.isFlagSet(.H));
+
+    incrementRegister(&processor, &processor.BC, Register.setHi, Register.getHi);
+    try expectEqual(0x02, processor.BC.getHi());
+    try expectEqual(false, processor.isFlagSet(.Z));
+    try expectEqual(false, processor.isFlagSet(.N));
+    try expectEqual(false, processor.isFlagSet(.H));
+
+    processor.BC.setHi(0xFF);
+    incrementRegister(&processor, &processor.BC, Register.setHi, Register.getHi);
+
+    try expectEqual(0x00, processor.BC.getHi());
+    try expectEqual(true, processor.isFlagSet(.Z));
+    try expectEqual(false, processor.isFlagSet(.N));
+    try expectEqual(true, processor.isFlagSet(.H));
+
+    processor.BC.setHi(0x0F);
+    incrementRegister(&processor, &processor.BC, Register.setHi, Register.getHi);
+    try expectEqual(0x10, processor.BC.getHi());
+    try expectEqual(false, processor.isFlagSet(.Z));
+    try expectEqual(false, processor.isFlagSet(.N));
+    try expectEqual(true, processor.isFlagSet(.H));
 }
