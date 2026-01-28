5ee33f7d20ee3e648f5722dd61ed8d3177b66bee
John Emerson Requizo <jr.jemerson14@gmail.com>
2026-01-28 22:20:05 +1100

    Add refactoring of instruction documentation

--- src/instruction.zig
+++ src/instruction.zig
@@ -17,6 +17,9 @@ const FlagCondition = enum {
 
 const utils = @import("utils.zig");
 
+
+/// Increment the contents of register reg by 1.
+/// Example: 0x05 -> DEC B
 pub fn inc_reg(
     proc: *Processor,
     reg: *Register,
@@ -28,6 +31,8 @@ pub fn inc_reg(
     if (sum.half_carry == 1) proc.setFlag(.H) else proc.unsetFlag(.H);
 }
 
+/// Decrement the contents of register reg by 1
+/// Example: 0x0D -> DEC C
 pub fn dec_reg(
     proc: *Processor,
     reg: *Register,
@@ -39,7 +44,7 @@ pub fn dec_reg(
     if (remainder.half_carry == 1) proc.setFlag(.H) else proc.unsetFlag(.H);
 }
 
-/// Increment register pair RR by 1
+/// Increment the contents of register pair rr by 1
 pub fn inc_rr(proc: *Processor, regPair: Processor.RegisterPair) void {
     switch (regPair) {
         .AF => proc.setAF(proc.getAF() + 1),
@@ -50,6 +55,8 @@ pub fn inc_rr(proc: *Processor, regPair: Processor.RegisterPair) void {
 }
 
 pub const load = struct {
+    /// Load the 8-bit immediate operand d8 into register reg.
+    /// Example: 0x06 -> LD B, d8
     pub fn reg_imm8(proc: *Processor, reg: *Register) void {
         reg.value = proc.fetch();
     }
@@ -82,7 +89,10 @@ pub const load = struct {
         dest.value = proc.memory.read(addr);
     }
 
-    /// Load into register pair rr immediate two byte data.
+    /// Load the 2 bytes of immediate data into register pair rr
+    /// The first byte of immediate data is the lower byte (i.e. bits 0-7), and
+    /// the second byte of immediate data is the higher byte (i.e., bits 8-15)
+    /// Example: 0x01 -> LD BC, d16
     pub fn rr_imm16(proc: *Processor, regPair: Processor.RegisterPair) void {
         switch (regPair) {
             .AF => {
@@ -112,12 +122,15 @@ pub const load = struct {
         proc.memory.write(mask.HI_MASK | imm, reg.value);
     }
 
-    /// Store the contents of a Register into the memory location specified by the register pair RR
+    /// Store the contents of a register reg into the memory location specified by the register pair rr.
+    /// Example: 0x12 -> LD (DE), A
     pub fn hlMem_reg(proc: *Processor, reg: *Register) void {
         proc.memory.write(proc.getHL(), reg.value);
     }
 
-    /// Store the contents of a Register into the memory location specified by the register pair RR
+    /// Store the contents of 8-bit immediate operand d8 in the memory location
+    /// specified by register pair rr.
+    /// Example: 0x36 -> LD (HL), d8
     pub fn rrMem_imm8(proc: *Processor, regPair: Processor.RegisterPair) void {
         const value = proc.fetch();
         switch (regPair) {
@@ -128,6 +141,9 @@ pub const load = struct {
         }
     }
 
+    /// Store the contents of register reg in the memory location specified by
+    /// register pair rr
+    /// Example: 0x02 -> LD (BC), A
     pub fn rrMem_reg(proc: *Processor, regPair: Processor.RegisterPair, reg: *Register) void {
         const addr = switch (regPair) {
             .AF => proc.getAF(),
@@ -168,14 +184,18 @@ pub const load = struct {
         proc.memory.write(addr, reg.value);
     }
 
-    /// Store into the immediate address the contents of special purpose registers
+    /// Store the lower byte of Special Purpose Register (SPR) at the address specified by the 16-bit
+    /// immediate operand a16, and store the upper byte of SPR at address a16 + 1.
+    /// Example: 0x08 -> LD (a16), SP
     pub fn imm16Mem_spr(proc: *Processor, val: u16) void {
-        const addr: u16 = utils.fromTwoBytes(proc.fetch(), proc.fetch());
+        const lo = proc.fetch();
+        const hi = proc.fetch();
+        const addr: u16 = utils.fromTwoBytes(lo, hi);
         proc.memory.write(addr, utils.getLoByte(val));
         proc.memory.write(addr + 1, utils.getHiByte(val));
     }
 
-    /// Load the 2 bytes of immediate data into special register.
+    /// Load the 2 bytes of immediate data into special purpose register (SPR).
     /// The first byte of immedaite data is the lower byte (i.e., bits 0-7), and the second byte of
     /// immediate data is the higher byte (i.e., bits 8-15).
     pub fn spr_imm16(proc: *Processor, spr: *u16) void {
@@ -206,7 +226,8 @@ pub const load = struct {
         spr.* = utils.fromTwoBytes(loReg.value, hiReg.value);
     }
 
-    /// Load the 8-bit contents of memory specified by register pair into register
+    /// Load the 8-bit contents of memory specified by register pair rr into register reg
+    /// Example: 0x0A -> LD A, (BC)
     pub fn reg_rrMem(proc: *Processor, reg: *Register, regPair: Processor.RegisterPair) void {
         switch (regPair) {
             .AF => reg.value = proc.memory.read(proc.getAF()),
@@ -216,24 +237,35 @@ pub const load = struct {
         }
     }
 
+    /// Store the contents of register reg into the memory location specified by register pair
+    /// HL, and simultaneously increment the contents of HL
+    /// Example: 0x22 -> LD (HL+), A
     pub fn hlMem_inc_reg(proc: *Processor, reg: *Register) void {
         const hl = proc.getHL();
         proc.memory.write(hl, reg.value);
         proc.setHL(hl +% 1);
     }
 
+    /// Store the contents of register reg into the memory location specified by register pair
+    /// HL, and simultaneously decrement the contents of HL.
     pub fn hlMem_dec_reg(proc: *Processor, reg: *Register) void {
         const hl = proc.getHL();
         proc.memory.write(hl, reg.value);
         proc.setHL(hl -% 1);
     }
 
+    /// Load the contents of memory specified by register pair rr into register reg, and simultaneously
+    /// increment the contents of HL.
+    /// Example: 0x2A -> LD A, (HL+)
     pub fn reg_hlMem_inc(proc: *Processor, reg: *Register) void {
         const hl = proc.getHL();
         reg.value = proc.memory.read(hl);
         proc.setHL(hl +% 1);
     }
 
+    /// Load the contents of memory specified by register pair HL into register reg, and
+    /// simultaneously decrement the contents of HL.
+    /// Example: 0x3A -> LD A, (HL-)
     pub fn reg_hlMem_dec(proc: *Processor, reg: *Register) void {
         const hl = proc.getHL();
         reg.value = proc.memory.read(hl);
@@ -242,15 +274,18 @@ pub const load = struct {
 };
 
 pub const controlFlow = struct {
-    /// Jump s8 steps fetch from immediate operand in the program counter (PC).
+    /// Jump s8 steps relative from the current address in the program counter (PC).
+    /// Example: 0x18 -> JR s8
     pub fn jump_rel_imm8(proc: *Processor) void {
         const offset = proc.fetch();
         proc.PC = utils.addOffset(proc.PC, offset);
     }
 
+    /// If the flag condition is met, jump s8 steps from the current address stored in the program counter (PC). If not, the
+    /// instruction following the current JP instruction is executed (as usual).
+    /// Example: 0x20 -> JR NZ, s8
     pub fn jump_rel_cc_imm8(proc: *Processor, condition: FlagCondition) void {
         const offset = proc.fetch();
-
         const cc: bool = switch(condition) {
             .Z => proc.isFlagSet(.Z),
             .NZ => !proc.isFlagSet(.Z),
@@ -267,12 +302,27 @@ pub const controlFlow = struct {
         }
     }
 
+    /// Load the 16-bit immediate operand a16 into the program counter (PC). a16 specifies the address of the
+    /// subsequently executed instruction.
+    /// The second byte of the object code (immediately following the opcode) corresponds to the lower-order
+    /// byte of a16 (bits 0-7), and the third byte of the object code corresponds to the higher-order byte
+    /// (bits 8-15).
+    /// Example: 0xC3 -> JP a16
     pub fn jump_imm16(proc: *Processor) void {
         const lo = proc.fetch();
         const hi = proc.fetch();
         proc.PC = utils.fromTwoBytes(lo, hi);
     }
 
+
+    /// Load the 16-bit immediate operand a16 into the program counter PC if the flag condition cc is met. If the
+    /// condition is met, then the subsequent instruction starts at address a16. If not, the contents of PC are
+    /// incremented, and the next instruction following the current JP instruction is executed (as usual).
+    ///
+    /// The second byte of the object code (immediately following the opcode) corresponds to the lower-order
+    /// byte of a16 (bits 0-7), and the third byte of the object code corresponds to the higher-order byte
+    /// (bits 8-15).
+    /// Example: 0xC2 -> JP NZ, a16
     pub fn jump_cc_imm16(proc: *Processor, condition: FlagCondition) void {
         const lo = proc.fetch();
         const hi = proc.fetch();
@@ -317,6 +367,14 @@ pub const controlFlow = struct {
         proc.PC = utils.fromTwoBytes(loReg.value, hiReg.value);
     }
 
+    /// Pop from the memory stack the program counter PC value pushed when the subroutine was called, returning
+    /// control to the source program.
+    /// The contents of the address specified by the stack pointer SP are loaded in the lower-order byte of PC,
+    /// and the contents of SP are incremented by 1. The contents of the address specified by the new SP value
+    /// are then loaded in the higher-order byte of PC, and the contents of SP are incremented by 1 again.
+    /// (The value of SP is 2 larger than before instruction execution.) The next instruction is fetched from
+    /// the address specified by the content of PC (as usual).
+    /// Example: 0xC9 -> RET
     pub fn ret(proc: *Processor) void {
         const lo: u8 = proc.popStack();
         const hi: u8 = proc.popStack();
@@ -324,6 +382,15 @@ pub const controlFlow = struct {
     }
 
 
+    /// If condition flag cc is met, control is returned to the source program by popping from the memory stack the program
+    /// counter PC value that was pushed to the stack when the subroutine was called.
+    ///
+    /// The contents of the address specified by the stack pointer SP are loaded in the lower-order byte of PC, and
+    /// the contents of SP are incremented by 1. The contents of the address specified by the new SP value are then
+    /// loaded in the higher-order byte of PC, and the contents of SP are incremented by 1 again. (The value of SP
+    /// is 2 larger than before instruction execution.) The next instruction is fetched from the address specified
+    /// by the content of PC (as usual).
+    /// Example: 0xC0 -> RET NZ
     pub fn ret_cc(proc: *Processor, condition: FlagCondition) void {
         const cc: bool = switch(condition) {
             .Z => proc.isFlagSet(.Z),
@@ -341,11 +408,24 @@ pub const controlFlow = struct {
         }
     }
 
+    /// Used when an interrupt-service routine finishes. The address for the return from the interrupt is loaded
+    /// in the program counter PC. The master interrupt enable flag is returned to its pre-interrupt status.
+    /// The contents of the address specified by the stack pointer SP are loaded in the lower-order byte of PC,
+    /// and the contents of SP are incremented by 1. The contents of the address specified by the new SP value
+    /// are then loaded in the higher-order byte of PC, and the contents of SP are incremented by 1 again. 
+    /// (THe value of SP is 2 larger than before instruction execution.) The next instruction is fetched from
+    /// the address specified by the content of PC (as usual).
+    /// Example: 0xD9 -> RETI
     pub fn reti(proc: *Processor) void {
         ret(proc);
         proc.IME = true;
     }
 
+    /// Pop the contents from the memory stack into register pair rr by doing the following:
+    /// 1. Load the contents of memory specified by stack pointer SP into the lower portion of rr.
+    /// 2. Add 1 to SP and load the contents from the new memory location into the upper portion rr.
+    /// 3. By the end, SP should be 2 more than its initial value.
+    /// Example: 0xC1 -> POP BC
     pub fn pop_rr(proc: *Processor, regPair: Processor.RegisterPair) void {
         var hiReg: *Register = undefined;
         var loReg: *Register = undefined;
@@ -371,6 +451,11 @@ pub const controlFlow = struct {
         hiReg.value = proc.popStack();
     }
 
+    /// Push the contents of register pair rr onto the memory stack by doing the following:
+    /// 1. Subtract 1 from the stack pointer SP, and put the contents of the higher portion of register pair
+    /// BC on on the stack.
+    /// 2. Subtract 1 from SP, and put the lower portion of register pair BC on the stack.
+    /// Example: 0xC5 -> PUSH BC
     pub fn push_rr(proc: *Processor, regPair: Processor.RegisterPair) void {
         var lo: *Register = undefined;
         var hi: *Register = undefined;
@@ -398,6 +483,18 @@ pub const controlFlow = struct {
         proc.pushStack(lo.value);
     }
 
+    /// In memory, push the program counter PC value corresponding to the address following the CALL instruction
+    /// to the 2 bytes following the byte specified by the current stack pointer SP. Then load the 16-bit
+    /// immediate operand a16 into PC.
+    /// The subroutine is placed after the location specified by the new PC value. When the subroutine finishes,
+    /// control is returned to the source program using a return instruction and by popping the starting address
+    /// of the next instruction (which was just pushed) and moving it to the PC.
+    /// With the push, the current value of SP is decremented by 1, and the higher-order byte of PC is loaded in
+    /// the memory address specified by the new SP value. The value of SP is then decremented by 1 again, and
+    /// the lower-order byte of PC is loaded in the memory address specified by that value of SP.
+    /// The lower-order byte of a16 is placed in byte 2 of the object code, and the higher-order byte is placed
+    /// in byte 3.
+    /// Example: 0xCD -> CALL a16
     pub fn call_imm16(proc: *Processor) void {
         const lo = proc.fetch();
         const hi = proc.fetch();
@@ -406,6 +503,13 @@ pub const controlFlow = struct {
         proc.PC = utils.fromTwoBytes(lo, hi);
     }
 
+    /// If condition flag is met, the program counter PC value corresponding to the memory location of the instruction
+    /// following the CALL instruction is pushed to the 2 bytes following the memory byte specified by the stack
+    /// pointer SP. The 16-bit immediate operand a16 is then loaded into PC.
+    ///
+    /// The lower-order byte of a16 is placed in byte 2 of the object code, and the higher-order byte is placed
+    /// in byte 3.
+    /// Example: 0xC4 -> CALL NZ, a16
     pub fn call_cc_imm16(proc: *Processor, condition: FlagCondition) void {
         const lo = proc.fetch();
         const hi = proc.fetch();
@@ -427,6 +531,18 @@ pub const controlFlow = struct {
         }
     }
 
+    /// Push the current value of the program counter PC onto the memory stack, and load into PC the 1th byte
+    /// of page 0 memory addresses, 0x00. The next instruction is fetched from the address specified by the new
+    /// content of PC (as usual).
+    /// With the push, the contents of the stack pointer SP are decremented by 1, and the higher-order byte of
+    /// PC is loaded in the memory address specified by the new SP value. The value of SP is then again
+    /// decremented by 1, and the lower-order byte of the PC is loaded in the memory address specified by that
+    /// value of SP.
+    /// The RST instruction can be used to jump to 1 of 8 addresses. Because all of the addresses are held in
+    /// page 0 memory, 0x00 is loaded in the higher-order byte of the PC, and 0x00 + (8 * index) is loaded in
+    /// the lower-order byte.
+    /// Example: 0xC7 -> RST 0 will load the address 0x0000
+    /// Example: 0xEF -> RST 5 will load the address 0x0028
     pub fn rst(proc: *Processor, index: u3) void {
         proc.pushStack(utils.getHiByte(proc.PC));
         proc.pushStack(utils.getLoByte(proc.PC));
