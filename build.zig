1234549708c35f181da9ce636614326572982f8b
John Emerson Requizo <jr.jemerson14@gmail.com>
2026-02-24 21:48:30 +1100

    I think this works somewhat. Would like to see if I can run individual unit tests without compiling the whole project though

--- build.zig
+++ build.zig
@@ -28,6 +28,12 @@ pub fn build(b: *std.Build) void {
     //     .optimize = optimize,
     // });
 
+    // const instructions_module = b.createModule(.{
+    //     .root_source_file = b.path("src/instructions/root.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+
     // We will also create a module for our other entry point, 'main.zig'.
     const exe_mod = b.createModule(.{
         // `root_source_file` is the Zig "entry point" of the module. If a module
@@ -35,6 +41,12 @@ pub fn build(b: *std.Build) void {
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const tests_module = b.createModule(.{
+        .root_source_file = b.path("src/test.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -99,6 +111,14 @@ pub fn build(b: *std.Build) void {
     //     .root_module = lib_mod,
     // });
 
+    // const instructions_unit_tests = b.addTest(.{
+    //     .root_module = instructions_module,
+    // });
+
+    const all_tests = b.addTest(.{
+        .root_module = tests_module,
+    });
+
     // const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const exe_unit_tests = b.addTest(.{
@@ -106,6 +126,8 @@ pub fn build(b: *std.Build) void {
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    // const run_instructions_unit_tests = b.addRunArtifact(instructions_unit_tests);
+    const run_all_test = b.addRunArtifact(all_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
@@ -113,4 +135,6 @@ pub fn build(b: *std.Build) void {
     const test_step = b.step("test", "Run unit tests");
     // test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
+    // test_step.dependOn(&run_instructions_unit_tests.step);
+    test_step.dependOn(&run_all_test.step);
 }
---
+++ src/instructions/root.zig
@@ -0,0 +1,1 @@
+pub const arithmetic = @import("arithmetic.zig");
