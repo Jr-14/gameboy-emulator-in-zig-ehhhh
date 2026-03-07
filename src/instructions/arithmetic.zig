bf0681960d8a6f9909e40296872d55a0690bd819
John Emerson Requizo <jr.jemerson14@gmail.com>
2026-03-08 10:58:53 +1100

    Move some test files around

--- src/instructions/arithmetic.zig
+++ src/instructions/arithmetic.zig
@@ -4,6 +4,8 @@ const Processor = @import("../processor_new.zig");
 const Memory = @import("../memory.zig");
 const utils = @import("../utils.zig");
 
+const expectEqual = std.testing.expectEqual;
+
 /// Increment the contents of register reg by 1.
 /// Example: 0x05 -> DEC B
 pub fn inc_reg8(
@@ -19,6 +21,47 @@ pub fn inc_reg8(
     proc.flags.negative = 0;
     proc.flags.zero = sum.value;
     proc.flags.half_carry = sum.half_carry;
+}
+
+test "inc_reg8" {
+    var memory: Memory = .init();
+    var processor = Processor.init(&memory, .{});
+
+    inc_reg8(&processor, &processor.B);
+
+    try expectEqual(0x01, processor.B.value);
+    try expectEqual(false, processor.isFlagSet(.Z));
+    try expectEqual(false, processor.isFlagSet(.N));
+    try expectEqual(false, processor.isFlagSet(.H));
+
+    inc_reg8(&processor, &processor.B);
+
+    try expectEqual(0x02, processor.B.value);
+    try expectEqual(false, processor.isFlagSet(.Z));
+    try expectEqual(false, processor.isFlagSet(.N));
+    try expectEqual(false, processor.isFlagSet(.H));
+
+    processor.B.value = 0xFF;
+    inc_reg8(&processor, &processor.B);
+
+    try expectEqual(0x00, processor.B.value);
+    try expectEqual(true, processor.isFlagSet(.Z));
+    try expectEqual(false, processor.isFlagSet(.N));
+    try expectEqual(true, processor.isFlagSet(.H));
+
+    processor.B.value = 0x0F;
+    inc_reg8(&processor, &processor.B);
+    try expectEqual(0x10, processor.B.value);
+    try expectEqual(false, processor.isFlagSet(.Z));
+    try expectEqual(false, processor.isFlagSet(.N));
+    try expectEqual(true, processor.isFlagSet(.H));
+
+    processor.E.value = 0x0F;
+    inc_reg8(&processor, &processor.E);
+    try expectEqual(0x10, processor.E.value);
+    try expectEqual(false, processor.isFlagSet(.Z));
+    try expectEqual(false, processor.isFlagSet(.N));
+    try expectEqual(true, processor.isFlagSet(.H));
 }
 
 /// Decrement the contents of register reg by 1
@@ -37,14 +80,81 @@ pub fn dec_reg8(
     proc.flags.half_carry = remainder.half_carry;
 }
 
+test "dec_reg8" {
+    var memory: Memory = .init();
+    var processor = Processor.init(&memory, .{ .D = 0x02 });
+
+    dec_reg8(&processor, &processor.D);
+    try expectEqual(0x01, processor.D.value);
+    try expectEqual(false, processor.isFlagSet(.Z));
+    try expectEqual(true, processor.isFlagSet(.N));
+    try expectEqual(false, processor.isFlagSet(.H));
+
+    dec_reg8(&processor, &processor.D);
+    try expectEqual(0x00, processor.D.value);
+    try expectEqual(true, processor.isFlagSet(.Z));
+    try expectEqual(true, processor.isFlagSet(.N));
+    try expectEqual(false, processor.isFlagSet(.H));
+
+    dec_reg8(&processor, &processor.D);
+    try expectEqual(0xFF, processor.D.value);
+    try expectEqual(false, processor.isFlagSet(.Z));
+    try expectEqual(true, processor.isFlagSet(.N));
+    try expectEqual(true, processor.isFlagSet(.H));
+}
+
 /// Increment the contents of register pair rr by 1
 pub fn inc_reg16(registerValue: *u16) void {
     registerValue.* +%= 1;
 }
 
+test "inc_reg16" {
+    var memory = Memory.init();
+    var processor = Processor.init(&memory, .{});
+
+    inc_reg16(&processor, .AF);
+    try expectEqual(1, processor.getAF());
+
+    processor.setAF(0xFFFF);
+    inc_reg16(&processor, .AF);
+    try expectEqual(0, processor.getAF());
+
+    processor.setBC(0x00FF);
+    inc_reg16(&processor, .BC);
+    try expectEqual(0x0100, processor.getBC());
+
+    processor.setDE(0x0101);
+    inc_reg16(&processor, .DE);
+    try expectEqual(0x0102, processor.getDE());
+
+    processor.setHL(0x0FFF);
+    inc_reg16(&processor, .HL);
+    try expectEqual(0x1000, processor.getHL());
+}
+
 /// Decrement the contents of register pair rr by 1
 pub fn dec_reg16(registerValue: *u16) void {
     registerValue.* -%= 1;
+}
+
+test "dec_reg16" {
+    var memory = Memory.init();
+    var processor = Processor.init(&memory, .{});
+
+    dec_reg16(&processor, .AF);
+    try expectEqual(0xFFFF, processor.getAF());
+
+    processor.setBC(0x0100);
+    dec_reg16(&processor, .BC);
+    try expectEqual(0x00FF, processor.getBC());
+
+    processor.setDE(0x0102);
+    dec_reg16(&processor, .DE);
+    try expectEqual(0x0101, processor.getDE());
+
+    processor.setHL(0x1000);
+    dec_reg16(&processor, .HL);
+    try expectEqual(0x0FFF, processor.getHL());
 }
 
 pub fn inc_sp(proc: *Processor) void {
@@ -104,6 +214,46 @@ pub fn add_reg8(proc: *Processor, registerValue: *u8) void {
     add_aux(proc, .{ .b = registerValue.* });
 }
 
+test "add_reg8" {
+    const PC: u16 = 0x0100;
+    const A: u8 = 0x14;
+    const B: u8 = 0x07;
+    var memory: Memory = .init();
+    var processor = Processor.init(&memory, .{ .PC = PC, .A = A, .B = B });
+
+    add_reg8(&processor, &processor.B);
+    try expectEqual(0x1B, processor.A.value);
+    try expectEqual(false, processor.isFlagSet(.Z));
+    try expectEqual(false, processor.isFlagSet(.N));
+    try expectEqual(false, processor.isFlagSet(.C));
+    try expectEqual(false, processor.isFlagSet(.H));
+
+    processor.A.value = 0xFF;
+    processor.C.value = 0xFF;
+    add_reg8(&processor, &processor.C);
+    try expectEqual(0xFE, processor.A.value);
+    try expectEqual(false, processor.isFlagSet(.Z));
+    try expectEqual(false, processor.isFlagSet(.N));
+    try expectEqual(true, processor.isFlagSet(.C));
+    try expectEqual(true, processor.isFlagSet(.H));
+
+    processor.D.value = 0x02;
+    add_reg8(&processor, &processor.D);
+    try expectEqual(0x00, processor.A.value);
+    try expectEqual(true, processor.isFlagSet(.Z));
+    try expectEqual(false, processor.isFlagSet(.N));
+    try expectEqual(true, processor.isFlagSet(.C));
+    try expectEqual(true, processor.isFlagSet(.H));
+
+    processor.E.value = 0x01;
+    add_reg8(&processor, &processor.E);
+    try expectEqual(0x01, processor.A.value);
+    try expectEqual(false, processor.isFlagSet(.Z));
+    try expectEqual(false, processor.isFlagSet(.N));
+    try expectEqual(false, processor.isFlagSet(.C));
+    try expectEqual(false, processor.isFlagSet(.H));
+}
+
 /// Add the contents of memory specified by register pair HL to the contents of register A, and store the results
 /// in register A.
 /// Example: 0x86 -> ADD A, (HL)
@@ -145,6 +295,21 @@ pub fn add_reg16_reg16(proc: *Processor, dest: *u16, src: *u16) void {
     proc.flags.negative = 0;
     proc.flags.carry = result.carry;
     proc.flags.half_carry = result.half_carry;
+}
+
+test "add_reg16_reg16" {
+    var memory = Memory.init();
+    var processor = Processor.init(&memory, .{
+        .B = 0x11,
+        .C = 0x5E,
+    });
+    
+    add_reg16_reg16(&processor, .HL, .BC);
+    try expectEqual(0x115E, processor.getHL());
+    try expectEqual(false, processor.isFlagSet(.Z));
+    try expectEqual(false, processor.isFlagSet(.N));
+    try expectEqual(false, processor.isFlagSet(.C));
+    try expectEqual(false, processor.isFlagSet(.H));
 }
 
 pub fn addc_imm8(proc: *Processor) void {
@@ -312,167 +477,3 @@ pub fn compare_imm8(proc: *Processor) void {
     compare_aux(proc, imm);
 }
 
-const expectEqual = std.testing.expectEqual;
-
-test "inc_reg8" {
-    var memory: Memory = .init();
-    var processor = Processor.init(&memory, .{});
-
-    inc_reg8(&processor, &processor.B);
-
-    try expectEqual(0x01, processor.B.value);
-    try expectEqual(false, processor.isFlagSet(.Z));
-    try expectEqual(false, processor.isFlagSet(.N));
-    try expectEqual(false, processor.isFlagSet(.H));
-
-    inc_reg8(&processor, &processor.B);
-
-    try expectEqual(0x02, processor.B.value);
-    try expectEqual(false, processor.isFlagSet(.Z));
-    try expectEqual(false, processor.isFlagSet(.N));
-    try expectEqual(false, processor.isFlagSet(.H));
-
-    processor.B.value = 0xFF;
-    inc_reg8(&processor, &processor.B);
-
-    try expectEqual(0x00, processor.B.value);
-    try expectEqual(true, processor.isFlagSet(.Z));
-    try expectEqual(false, processor.isFlagSet(.N));
-    try expectEqual(true, processor.isFlagSet(.H));
-
-    processor.B.value = 0x0F;
-    inc_reg8(&processor, &processor.B);
-    try expectEqual(0x10, processor.B.value);
-    try expectEqual(false, processor.isFlagSet(.Z));
-    try expectEqual(false, processor.isFlagSet(.N));
-    try expectEqual(true, processor.isFlagSet(.H));
-
-    processor.E.value = 0x0F;
-    inc_reg8(&processor, &processor.E);
-    try expectEqual(0x10, processor.E.value);
-    try expectEqual(false, processor.isFlagSet(.Z));
-    try expectEqual(false, processor.isFlagSet(.N));
-    try expectEqual(true, processor.isFlagSet(.H));
-}
-
-test "dec_reg8" {
-    var memory: Memory = .init();
-    var processor = Processor.init(&memory, .{ .D = 0x02 });
-
-    dec_reg8(&processor, &processor.D);
-    try expectEqual(0x01, processor.D.value);
-    try expectEqual(false, processor.isFlagSet(.Z));
-    try expectEqual(true, processor.isFlagSet(.N));
-    try expectEqual(false, processor.isFlagSet(.H));
-
-    dec_reg8(&processor, &processor.D);
-    try expectEqual(0x00, processor.D.value);
-    try expectEqual(true, processor.isFlagSet(.Z));
-    try expectEqual(true, processor.isFlagSet(.N));
-    try expectEqual(false, processor.isFlagSet(.H));
-
-    dec_reg8(&processor, &processor.D);
-    try expectEqual(0xFF, processor.D.value);
-    try expectEqual(false, processor.isFlagSet(.Z));
-    try expectEqual(true, processor.isFlagSet(.N));
-    try expectEqual(true, processor.isFlagSet(.H));
-}
-
-test "inc_reg16" {
-    var memory = Memory.init();
-    var processor = Processor.init(&memory, .{});
-
-    inc_reg16(&processor, .AF);
-    try expectEqual(1, processor.getAF());
-
-    processor.setAF(0xFFFF);
-    inc_reg16(&processor, .AF);
-    try expectEqual(0, processor.getAF());
-
-    processor.setBC(0x00FF);
-    inc_reg16(&processor, .BC);
-    try expectEqual(0x0100, processor.getBC());
-
-    processor.setDE(0x0101);
-    inc_reg16(&processor, .DE);
-    try expectEqual(0x0102, processor.getDE());
-
-    processor.setHL(0x0FFF);
-    inc_reg16(&processor, .HL);
-    try expectEqual(0x1000, processor.getHL());
-}
-
-test "dec_reg16" {
-    var memory = Memory.init();
-    var processor = Processor.init(&memory, .{});
-
-    dec_reg16(&processor, .AF);
-    try expectEqual(0xFFFF, processor.getAF());
-
-    processor.setBC(0x0100);
-    dec_reg16(&processor, .BC);
-    try expectEqual(0x00FF, processor.getBC());
-
-    processor.setDE(0x0102);
-    dec_reg16(&processor, .DE);
-    try expectEqual(0x0101, processor.getDE());
-
-    processor.setHL(0x1000);
-    dec_reg16(&processor, .HL);
-    try expectEqual(0x0FFF, processor.getHL());
-}
-
-test "add_reg8" {
-    const PC: u16 = 0x0100;
-    const A: u8 = 0x14;
-    const B: u8 = 0x07;
-    var memory: Memory = .init();
-    var processor = Processor.init(&memory, .{ .PC = PC, .A = A, .B = B });
-
-    add_reg8(&processor, &processor.B);
-    try expectEqual(0x1B, processor.A.value);
-    try expectEqual(false, processor.isFlagSet(.Z));
-    try expectEqual(false, processor.isFlagSet(.N));
-    try expectEqual(false, processor.isFlagSet(.C));
-    try expectEqual(false, processor.isFlagSet(.H));
-
-    processor.A.value = 0xFF;
-    processor.C.value = 0xFF;
-    add_reg8(&processor, &processor.C);
-    try expectEqual(0xFE, processor.A.value);
-    try expectEqual(false, processor.isFlagSet(.Z));
-    try expectEqual(false, processor.isFlagSet(.N));
-    try expectEqual(true, processor.isFlagSet(.C));
-    try expectEqual(true, processor.isFlagSet(.H));
-
-    processor.D.value = 0x02;
-    add_reg8(&processor, &processor.D);
-    try expectEqual(0x00, processor.A.value);
-    try expectEqual(true, processor.isFlagSet(.Z));
-    try expectEqual(false, processor.isFlagSet(.N));
-    try expectEqual(true, processor.isFlagSet(.C));
-    try expectEqual(true, processor.isFlagSet(.H));
-
-    processor.E.value = 0x01;
-    add_reg8(&processor, &processor.E);
-    try expectEqual(0x01, processor.A.value);
-    try expectEqual(false, processor.isFlagSet(.Z));
-    try expectEqual(false, processor.isFlagSet(.N));
-    try expectEqual(false, processor.isFlagSet(.C));
-    try expectEqual(false, processor.isFlagSet(.H));
-}
-
-test "add_reg16_reg16" {
-    var memory = Memory.init();
-    var processor = Processor.init(&memory, .{
-        .B = 0x11,
-        .C = 0x5E,
-    });
-    
-    add_reg16_reg16(&processor, .HL, .BC);
-    try expectEqual(0x115E, processor.getHL());
-    try expectEqual(false, processor.isFlagSet(.Z));
-    try expectEqual(false, processor.isFlagSet(.N));
-    try expectEqual(false, processor.isFlagSet(.C));
-    try expectEqual(false, processor.isFlagSet(.H));
-}
